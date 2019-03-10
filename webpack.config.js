bede1ad0926259e527e5d845ba4853a8335ec182
danielrosado <dj.rosado@alumnos.urjc.es>
2019-03-10 17:06:29 +0100

    #2 - Update webpack.config.js mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,49 @@
 /*
- *   Webpack config script for Privacy Shield
+ *   Webpack config script for Privacy Shield development
  */
 
+"use strict";
+
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
-const SRC_JS_DIR = path.resolve(__dirname, "src/js");
-const BUILD_JS_DIR = path.resolve(__dirname, "dist/js");
+const SRC_DIR = path.resolve(__dirname, "src");
+const DIST_DIR = path.resolve(__dirname, "dist");
 
-const mode = "development";
+const buildPlugins = [
+  new CopyPlugin([
+    {
+      from: SRC_DIR,
+      to: DIST_DIR,
+      ignore: ["*.js", "*.css"],
+    },
+    {
+      from: `${SRC_DIR}/css`,
+      to: `${DIST_DIR}/css`,
+    },
+    {
+      from: `${SRC_DIR}/js`,
+      to: `${DIST_DIR}/js`,
+      ignore: ["webrequest.js"],
+    },
+  ]),
+];
 
-module.exports = {
-  mode: mode,
-  devtool: "cheap-module-source-map", // https://stackoverflow.com/questions/48047150/refused-to-evaluate-a-string-as-javascript-because-unsafe-eval-is-not-an-allow
+const config = {
   entry: {
-    webrequest: [SRC_JS_DIR + "/webrequest.js"],
+    webrequest: [`${SRC_DIR}/js/webrequest.js`],
   },
   output: {
-    path: BUILD_JS_DIR,
+    path: `${DIST_DIR}/js`,
     filename: "[name].js",
   },
+  plugins: buildPlugins,
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === "development") {
+    config.devtool = "cheap-module-source-map"; // https://stackoverflow.com/questions/48047150/refused-to-evaluate-a-string-as-javascript-because-unsafe-eval-is-not-an-allow
+  }
+
+  return config;
 };
