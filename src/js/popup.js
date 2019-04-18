745cd1cdb8ef7a5330ddb3209d517e15bd81545b
danielrosado <dj.rosado@alumnos.urjc.es>
2019-04-18 19:47:33 +0200

    #2 - Updated popup, now it shows the third-party domains of the current web site (closes #2)

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,1 +1,40 @@
-console.log("Hello Privacy Shield!");
+'use strict';
+
+import {GET_THIRD_PARTY_DOMAINS_TAB} from './utils/constants';
+
+let popupData = {};
+
+/**
+ * Prints popup info
+ */
+function printPopup() {
+  for (let domain of popupData.domains) {
+    let p = document.createElement('p');
+    p.innerText = domain;
+    document.querySelector('body').appendChild(p);
+  }
+}
+
+/**
+ * Initializes the popup script
+ */
+function init() {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    const tab = tabs[0];
+    chrome.runtime.sendMessage(
+      {
+        'type': GET_THIRD_PARTY_DOMAINS_TAB,
+        'tabId': tab.id,
+      },
+      (response) => {
+        popupData.domains = response;
+        printPopup();
+      },
+    );
+  });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  init();
+});
